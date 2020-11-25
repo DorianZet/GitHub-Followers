a2bd417b701532cde910dd91c0d213bf78a3c5e0
DorianZet <62523613+DorianZet@users.noreply.github.com>
2020-11-25 10:56:57 +0100

    Show basic info about user in UserInfoVC and added view containers
    - add GFUserInfoHeaderVC with 'user' initializer to present the info about them
    - add 3 container views to UserInfoVC to show the info about the user
    - add GFUserHeaderInfoVC to headerView in UserInfoVC, showing the info about the user
    - refactor viewDidLoad() in UserInfoVC (separate the network call, UI layout and navigation controller configuration)
    - create GFItemInfoView to show the counts of the user's repos, gists, followers and following (not used yet)

---
+++ GitHubFollowers/Utilities/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  GitHubFollowers
+//
+//  Created by Mateusz Zacharski on 25/11/2020.
+//
+
+import Foundation
+
+enum SFSymbols { // the names of SFSymbols files.
+    static let location = "mappin.and.ellipse" // making the property static so we can use it in the other classes.
+    static let repos = "folder"
+    static let gists = "text.alignleft"
+    static let followers = "heart"
+    static let following = "person.2"
+}
